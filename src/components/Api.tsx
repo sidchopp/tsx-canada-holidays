3575b859a3216c635bbfd08f796432f0b4e0956d
sidchopp <sidchopp@gmail.com>
2022-07-30 09:12:45 -0600

    axios is removed by fetch using async await

--- src/components/Api.tsx
+++ src/components/Api.tsx
@@ -1,24 +1,28 @@
-import axios from "axios";
+import { useState, useEffect } from "react";
 
 const Api = () => {
-  const options = {
-    method: "GET",
-    url: "https://canada-holidays.p.rapidapi.com/api/v1/provinces",
-    params: { optional: "false", year: "2022" },
-    headers: {
-      "X-RapidAPI-Key": "16a6a19bc4mshed2926e4f407384p15a805jsn3c1b8e1b24d6",
-      "X-RapidAPI-Host": "canada-holidays.p.rapidapi.com",
-    },
+  const getResponse = async () => {
+    const response = await fetch(
+      "https://canada-holidays.p.rapidapi.com/api/v1/provinces?optional=false&year=2022",
+      {
+        method: "GET",
+        headers: {
+          "X-RapidAPI-Key":
+            "16a6a19bc4mshed2926e4f407384p15a805jsn3c1b8e1b24d6",
+          "X-RapidAPI-Host": "canada-holidays.p.rapidapi.com",
+        },
+      }
+    );
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    console.log(data);
   };
 
-  axios
-    .request(options)
-    .then(function (response) {
-      console.log(response.data);
-    })
-    .catch(function (error) {
-      console.error(error);
-    });
+  useEffect(() => {
+    getResponse();
+  }, []);
 
   return <div>Api</div>;
 };
