a5ac419b5637ef387033fc9395b6def48fcf06f4
sidchopp <sidchopp@gmail.com>
2022-07-31 15:56:48 -0600

    Card header modified

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -3,8 +3,8 @@ import Typography from "@mui/material/Typography";
 const Heading = () => {
   return (
     <div className="header">
-      <Typography gutterBottom variant="h4" align="center">
-        List of Canadian Holidays
+      <Typography gutterBottom variant="h5" align="center">
+        Canadian Public and Federal Holidays for all 13 provinces
       </Typography>
     </div>
   );
