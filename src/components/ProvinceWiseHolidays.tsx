ba6b4441e4db0344a8a5ed1a497786dc78680ccf
sidchopp <sidchopp@gmail.com>
2025-05-10 13:53:48 -0600

    toggleLanguage removed from cards and moved to header

--- src/components/ProvinceWiseHolidays.tsx
+++ src/components/ProvinceWiseHolidays.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { AppProps } from "../interfaces/Interface";
 
 import {
@@ -8,110 +7,94 @@ import {
   Grid,
   Typography,
   Container,
-  Button,
 } from "@mui/material";
 
-const ProvinceWiseHolidays = ({ provData }: AppProps) => {
-  const [english, setEnglish] = useState(true);
-
-  const changeLanguage = () => {
-    setEnglish(!english);
-  };
-
+const ProvinceWiseHolidays = ({ provData, english }: AppProps) => {
   return (
-    <div>
-      <Container sx={{ py: 8 }} maxWidth="xl">
-        <Grid container spacing={4}>
-          {provData.map((card) => (
-            <Grid
-              item
-              key={card.id}
-              xs={12}
-              sm={6}
-              md={4}
-              style={{ display: "flex" }}
+    <Container sx={{ py: 8 }} maxWidth="xl">
+      <Grid container spacing={4}>
+        {provData.map((card) => (
+          <Grid
+            item
+            key={card.id}
+            xs={12}
+            sm={6}
+            md={4}
+            style={{ display: "flex" }}
+          >
+            <Card
+              variant="outlined"
+              sx={{
+                height: "100%",
+                width: "100%",
+                display: "flex",
+                flexDirection: "column",
+              }}
+              className="card"
             >
-              <Card
-                variant="outlined"
-                sx={{
-                  height: "100%",
-                  width: "100%",
-                  display: "flex",
-                  flexDirection: "column",
-                }}
-                className="card"
-              >
-                <CardHeader
-                  title={
-                    <Typography variant="h6" component="div">
-                      <span className="province-name">
-                        {english ? card.nameEn : card.nameFr}
-                      </span>
-                    </Typography>
-                  }
-                  subheader={
-                    <Typography
-                      variant="caption"
-                      display="block"
-                      color="text.secondary"
-                    >
-                      <span className="holidays-number">
-                        <i>
-                          {card.holidays.length}{" "}
-                          {english ? "holidays" : "vacances"}
-                        </i>
-                      </span>
-                    </Typography>
-                  }
-                  action={
-                    <Button onClick={changeLanguage} component="label">
-                      <span className="language-button">
-                        {english ? "FR" : "EN"}
-                      </span>
-                    </Button>
-                  }
-                />
-                <CardContent sx={{ flexGrow: 1 }}>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    {card.holidays.map((holiday) => {
-                      return (
-                        <div key={holiday.id}>
-                          {holiday.date === card.nextHoliday.date ? (
-                            <Typography
-                              variant="subtitle2"
-                              gutterBottom
-                              component="div"
-                              color="primary"
-                            >
-                              <span className=" next-holiday">
-                                {holiday.date.slice(-5)}:{" "}
-                                {english ? holiday.nameEn : holiday.nameFr}
-                              </span>
-                            </Typography>
-                          ) : (
-                            <Typography
-                              variant="caption"
-                              display="block"
-                              gutterBottom
-                            >
-                              <span className="holidays">
-                                {" "}
-                                {holiday.date.slice(-5)}:{" "}
-                                {english ? holiday.nameEn : holiday.nameFr}
-                              </span>
-                            </Typography>
-                          )}
-                        </div>
-                      );
-                    })}
+              <CardHeader
+                title={
+                  <Typography variant="h6" component="div">
+                    <span className="province-name">
+                      {english ? card.nameEn : card.nameFr}
+                    </span>
                   </Typography>
-                </CardContent>
-              </Card>
-            </Grid>
-          ))}
-        </Grid>
-      </Container>
-    </div>
+                }
+                subheader={
+                  <Typography
+                    variant="caption"
+                    display="block"
+                    color="text.secondary"
+                  >
+                    <span className="holidays-number">
+                      <i>
+                        {card.holidays.length}{" "}
+                        {english ? "holidays" : "vacances"}
+                      </i>
+                    </span>
+                  </Typography>
+                }
+              />
+              <CardContent sx={{ flexGrow: 1 }}>
+                <Typography gutterBottom variant="h5" component="h2">
+                  {card.holidays.map((holiday) => {
+                    return (
+                      <div key={holiday.id}>
+                        {holiday.date === card.nextHoliday.date ? (
+                          <Typography
+                            variant="subtitle2"
+                            gutterBottom
+                            component="div"
+                            color="primary"
+                          >
+                            <span className=" next-holiday">
+                              {holiday.date.slice(-5)}:{" "}
+                              {english ? holiday.nameEn : holiday.nameFr}
+                            </span>
+                          </Typography>
+                        ) : (
+                          <Typography
+                            variant="caption"
+                            display="block"
+                            gutterBottom
+                          >
+                            <span className="holidays">
+                              {" "}
+                              {holiday.date.slice(-5)}:{" "}
+                              {english ? holiday.nameEn : holiday.nameFr}
+                            </span>
+                          </Typography>
+                        )}
+                      </div>
+                    );
+                  })}
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
   );
 };
 
